72f43afddc642c56d7591391549948e0f5cd2ccb
nick <nickyang@ecolux-tech.com>
2026-01-06 11:16:59 +0800

    twdToCanvas

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ import * as Util from "./indexes/index_utility.js";
 //=> canvas point 
 //<= wgs座標
 //==================================================
-const tapPoint = new Models.ModelPoint(184, 39);
+const tapPoint = new Models.ModelPoint(111,215);
 const imgSize = new Models.ModelImgSize({ width: 250, height: 250 });
 const angle = -135;
 const scale = 0.15;
@@ -28,32 +28,26 @@ const tapInfo = new Models.ModelTapInfo(
 );
 const wgs = Util.MapUtility.canvasToWgs(tapInfo);
 console.log(wgs.lat, ",", wgs.lng);
-//==================================================
+
 //==================================================
 //ex2. 霧峰球場，第12洞 搭配 fairway2, green (rotate_20251119_114231.jpg)
 //=> twd97 point,
 //<= canvas point
 //把Wgs座標放進canvas上
-const targetWgs =  new Models.ModelGpsPoint(24.029116139280383, 120.75142754596479);
+const targetWgs =  new Models.ModelGpsPoint(24.032524734103653 , 120.75087838025009);
 const targetTwd97 = Util.toTwd97.getLocation(targetWgs.lat, targetWgs.lng);
-
-// const targetTwd97 = new Models.ModelTwd97Point(224712.42805275286,2658269.26535896);  
 const mapProjectInfo = new Models.ModelProjectInfo(
   new Models.ModelTwd97Point(targetTwd97.x, targetTwd97.y),
-  new Models.ModelTwd97Point(224362.0,2658617.6049999995),
+  new Models.ModelTwd97Point(224543.62,2658970.12),
   //TODO: 待調整
-  new Models.ModelPoint(213.33333333333334,363.16472114137486) ,
-  -110,
+  new Models.ModelPoint(43.60000000005425, 1320.6999999998768),    
+  -135,
   0.15,
-  0.20752269779507135 
+  1 
 );
 const res = Util.MapUtility.twdToCanvas(mapProjectInfo);
 console.log(res.x, ",", res.y);
-// res=> 232.078071 , 67.44081
-//==================================================
 
-
-//==================================================
 //ex.3 
 const tapPoint2 = new Models.ModelPoint(0, 10);
 const scale2 = 0.15;
@@ -62,15 +56,3 @@ console.log( Util.MapUtility.meterToYard(dis ));
 //==================================================
 
 
-
-// // 旋轉 base case , output => (0,1)
-// const mapInfo = new Models.ModelProjectInfo(
-//   new Models.ModelTwd97Point(1, 0),
-//   new Models.ModelTwd97Point(0, 0),
-//   new Models.ModelPoint(0, 0),
-//   90,
-//   1,
-//   1
-// );
-// const res0 = Util.MapUtility.twdToCanvas(mapInfo);
-// console.log(res0.x, ",", res0.y);
--- models/model_tap_info.js
+++ models/model_tap_info.js
@@ -6,6 +6,6 @@ export class ModelTapInfo {
     this.angle = angle;
     this.scale = scale;
     this.originTwd97Point = originTwd97Point;
-    this.centerPoint = centerPoint;
+    this.centerPoint = centerPoint; // 因為是從子圖再裁切，所以旋轉中心點要用父圖的中心點
   }
 }
